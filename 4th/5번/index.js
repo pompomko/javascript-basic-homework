a9a9d90c81b3c8b86ef81aed411e33a09e058669
pompomko <sdw12334@gmail.com>
2023-11-14 20:43:10 +0900

    4회차 과제 완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,40 @@
-// 여기에 1번 문제의 답을 작성하세요.
+// 여기에 1번 문제의 답을 작성하세요.async await 문법으로 바꿔보세요.
 
-// 여기에 2번 문제의 답을 작성하세요.
+// fetch("https://dummyjson.com/products")
+//   .then((res) => res.json())
+//   .then(console.log);
+
+const 아무이름 = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  const data = await res.json();
+  console.log("아무이름", data);
+  return data;
+};
+아무이름();
+
+// 여기에 2번 문제의 답을 작 성하세요.
+// fetch("https://dummyjson.com/products/add", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     title: "BMW Pencil",
+//     /* other product data */
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then(console.log);
+
+const 아무이름2 = async () => {
+  const res = await fetch("https://dummyjson.com/products/add", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: "BMW Pencil",
+      /* other product data */
+    }),
+  });
+  const data1 = await res.json();
+  console.log("아무이름2", data1);
+  return data1;
+};
+아무이름2();
